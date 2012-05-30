6c09e4bb3a3f0f8fb63d683819b5e3327ab38ba5
Andrew Hammond <andrew.george.hammond@gmail.com>
2012-05-30 15:35:57 -0700

    modified to work on RackSpace virtual servers and use graphite_reporter module

--- iostat_to_graphite.rb
+++ iostat_to_graphite.rb
@@ -1,30 +1,23 @@
-#!/opt/csw/bin/ruby
+#!/usr/bin/env ruby
 #
 # run with something like iostat -nx 10 | iostat_to_graphite.rb
-#
+# modified to work on rackspace virtual servers
 
-require 'socket'
+# http://www.cyberciti.biz/tips/linux-disk-performance-monitoring-howto.html
 
-graphite_host='graphite'
-graphite_port=2003
+# Get this from https://github.com/SmartReceipt/graphite_reporter
+require 'graphite_reporter'
 
-hostname=`hostname`.chomp
-
+ENV['GRAPHITE_HOST'] = 'graphite01'
+ENV['GRAPHITE_PORT'] = '2005'
+ENV['GRAPHITE_PROTOCOL'] = 'upd'
 
 ARGF.each do |line|
-    tstamp = Time.now.to_i
-    if (line !~ /extended device|r\/s/) then
-       s = TCPSocket.open(graphite_host, graphite_port)
-       parts = line.split(' ')
-       device = parts[10].gsub(/[\/\(\): ]/,'_')
-       prefix = "servers.#{hostname}.iostat.#{device}"
-       s.puts "#{prefix}.reads_sec #{parts[0]} #{tstamp}" 
-       s.puts "#{prefix}.writes_sec #{parts[1]} #{tstamp}" 
-       s.puts "#{prefix}.kreads_sec #{parts[2]} #{tstamp}" 
-       s.puts "#{prefix}.kwrites_sec #{parts[3]} #{tstamp}" 
-       s.puts "#{prefix}.pc_wait #{parts[8]} #{tstamp}" 
-       s.puts "#{prefix}.pc_busy #{parts[9]} #{tstamp}" 
-       s.close
-    
+  if (line ~ /^xvd/) then
+    values = line.split(' ')
+    device_name = values.pop
+    #prefix = "#{hostname}.iostat.#{values.pop}"
+    %w{rrqmps wrpmpw rps wps rsecps wsecps avgrqsz avgqusz await svctm putil}.each do |stat|
+      GraphiteReporter :name => "iostat.#{device_name}", :key => stat :value => values.pop
     end
 end
